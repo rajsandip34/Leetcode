bb584d9bd561e6d4f9f6b2d76cdfd8966cd5a980
sandip raj <rajsandip1979@gmail.com>
2025-08-11 13:53:58 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.5 MB (54.61%)

---
+++ 0007-reverse-integer/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int rev=0;
+        int digit;
+        
+        while(x!=0){
+            digit =x%10;
+            if(rev>INT_MAX/10 || (rev==INT_MAX/10&&digit>7)) return 0;
+            if(rev<INT_MIN/10 || (rev==INT_MIN/10&&digit<-8)) return 0;
+            rev=rev*10+digit;
+            x/=10;
+
+            }
+        return rev;
+        
+        
+        
+    }
+};

eb28e1d763a4fdbcc0c68ef9144c77220c98c6a2
sandip raj <rajsandip1979@gmail.com>
2025-08-12 17:53:54 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 15.3 MB (6.42%)

---
+++ 0898-transpose-matrix/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        int m= matrix.size();
+        int n = matrix[0].size();
+        
+        vector<vector<int>> matrix2 (n,vector<int>(m));
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                
+                matrix2[j][i]= matrix[i][j];
+                
+                
+            }
+        }
+        return matrix2;
+    }
+};

9c33535d069d9c17427f18a0f8eed44a3326da9c
sandip raj <rajsandip1979@gmail.com>
2025-11-15 08:18:49 +0000

    Sync LeetCode submission Runtime - 5 ms (26.23%), Memory - 9.9 MB (26.06%)

--- 0242-valid-anagram/solution.cpp
+++ 0242-valid-anagram/solution.cpp
@@ -1,10 +1,28 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if(s==t) return true;
-        else return false;
-    }
+        if(s.length()!=t.length()) return false;
+        unordered_map<char,int> map1; //for string s
         
+        for(char x:s) map1[x]++;
+        for(auto x:t){
+            char ch = x;
+            if(map1.find(ch)!=map1.end()){
+                map1[ch]--;
+                if(map1[ch]==0) map1.erase(ch);
+            }else{
+                return false;
+
+            } 
+        }
+        if(map1.empty()) return true;
+        
+        return false;
+
+
+    }
 };
+            
+        
+            
+        

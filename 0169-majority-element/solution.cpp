ff727e5c618c2dc7abe11e8860dc9335b71303dc
sandip raj <rajsandip1979@gmail.com>
2025-11-15 15:31:55 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 28.3 MB (35.96%)

--- 0169-majority-element/solution.cpp
+++ 0169-majority-element/solution.cpp
@@ -3,7 +3,6 @@ public:
     int majorityElement(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         int n=nums.size();
-        if(n==1) return nums[0];
         return nums[n/2];
         
     }

cf275d631b96381a809c0b6ecea3c4c7c3cbd67c
sandip raj <rajsandip1979@gmail.com>
2026-04-07 17:53:30 +0000

    Sync LeetCode submission Runtime - 13 ms (32.20%), Memory - 7.7 MB (88.80%)

--- 1013-fibonacci-number/solution.cpp
+++ 1013-fibonacci-number/solution.cpp
@@ -1,17 +1,8 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n==0) return 0;
-        if(n==1) return 1;
-        int a=0,b=1;
-        int c;
-        for(int i=2;i<=n;i++){
-            c=a+b;
-            a=b;
-            b=c;
-        }
-        return c;
         
-
+        if(n<=1) return n;
+        return fib(n-1)+fib(n-2);
     }
 };

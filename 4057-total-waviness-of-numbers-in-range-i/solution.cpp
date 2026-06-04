e6ae96e619d825e35779dec1d07f1c8005aac202
sandip raj <rajsandip1979@gmail.com>
2026-06-04 06:11:38 +0000

    Sync LeetCode submission Runtime - 38 ms (75.60%), Memory - 9.3 MB (83.75%)

--- 4057-total-waviness-of-numbers-in-range-i/solution.cpp
+++ 4057-total-waviness-of-numbers-in-range-i/solution.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
     int totalWaviness(int num1, int num2) {
-        int waviness=0;
+        int ans=0;
         for(int i=num1;i<=num2;i++){
-            string a = to_string(i);
-            if(a.length()<=2) continue;
-            for(int j=0;j<=a.length()-3;j++){
-                if((a[j]<a[j+1]) && (a[j+1]>a[j+2])) waviness++;
-                if((a[j]>a[j+1]) && (a[j+1]<a[j+2])) waviness++;
+            
+            string num = to_string(i);
+            if(num.length()<3) continue;
+            char prev = num[0];
+            for(int j=1;j<num.length()-1;j++){
+                int curr = num[j];
+                int next=num[j+1];
+                //peak
+                if(prev<curr && curr>next) ans++;
+                //valley
+                else if(prev>curr && curr<next) ans++;
+                prev= curr;
             }
         }
-        return waviness;
-            
+        return ans;
     }
 };

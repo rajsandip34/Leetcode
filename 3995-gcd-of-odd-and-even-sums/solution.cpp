caf4415262d0fbc587ed2fb48551b0f04a989424
sandip raj <rajsandip1979@gmail.com>
2026-07-15 08:17:52 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.6 MB (10.21%)

--- 3995-gcd-of-odd-and-even-sums/solution.cpp
+++ 3995-gcd-of-odd-and-even-sums/solution.cpp
@@ -1,29 +1,7 @@
 class Solution {
 public:
-    int sumEven(int n){
-        int sum=n*(n-1);
-        return sum;
-    }
-        
-        
-        
-        
-    int sumOdd(int n){
-        int sum=n*n;
-        return sum;
-    }
-    int gcd(int a,int b){
-        if(b==0) return a;
-        if(a%b==0){
-            return b;
-        }
-        return gcd(b,a%b);
-    }
     
     int gcdOfOddEvenSums(int n) {
-        int a =  sumEven(n);
-        int b =  sumOdd(n);
-        int ans=gcd(max(a,b),min(a,b));
-        return ans;
+        return n;
     }
 };

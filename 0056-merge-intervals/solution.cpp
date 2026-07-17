fa770700c9e9410c38c60a60c557b2639f3dc455
sandip raj <rajsandip1979@gmail.com>
2026-07-17 12:36:52 +0000

    Sync LeetCode submission Runtime - 3 ms (88.82%), Memory - 24 MB (30.57%)

--- 0056-merge-intervals/solution.cpp
+++ 0056-merge-intervals/solution.cpp
@@ -1,30 +1,24 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        int n= intervals.size();
-        if(n==1 || n==0) return intervals;
-        sort(intervals.begin(),intervals.end());
+        
+        int n =intervals.size();
+        if(n==0 || n==1) return intervals;
         vector<vector<int>> ans;
+        sort(intervals.begin(),intervals.end());
         ans.push_back(intervals[0]);
-
-        int j=0;
+        int k=0;
         for(int i=1;i<n;i++){
-            int left = ans[j][0];
-            int right= ans[j][1];
-            int l = intervals[i][0];
-            int r = intervals[i][1];
-            if((l>=left && l<=right) && (r>=left && r<=right)){
-                continue;
-            }
-            else if(l>=left && l<=right) {
-                ans[j][0]=left;
-                ans[j][1] = intervals[i][1];
+            int l = ans[k][0];
+            int r = ans[k][1];
+            int left = intervals[i][0];
+            int right = intervals[i][1];
+            if(r>=left){
+                ans[k][1] = max(r,right);
             }else{
                 ans.push_back(intervals[i]);
-                j++;
+                k++;
             }
-            
-
         }
         return ans;
     }

32a4f9762cef54b660bc9c26e370bc1db294c824
sandip raj <rajsandip1979@gmail.com>
2026-08-08 14:36:32 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 7.7 MB (85.97%)

---
+++ 1236-n-th-tribonacci-number/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+ 
+
+    int tribonacci(int n) {
+        int a=0,b=1,c=1;
+        if(n==0) return a;
+        if(n==1) return b;
+        if(n==2) return c;
+        int d=0;
+        for(int i=3;i<=n;i++){
+            d=a+b+c;
+            a=b;
+            b=c;
+            c=d;
+        }
+        return d;
+        
+    }
+};

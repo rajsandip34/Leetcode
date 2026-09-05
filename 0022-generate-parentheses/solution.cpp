72199d3068d62ffe8fdf737d1247ba94a717eb07
sandip raj <rajsandip1979@gmail.com>
2026-09-05 09:44:32 +0000

    Sync LeetCode submission Runtime - 2 ms (79.71%), Memory - 15.9 MB (28.48%)

--- 0022-generate-parentheses/solution.cpp
+++ 0022-generate-parentheses/solution.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     vector<string> ans;
-    
     void f(int n,string s,int open,int close){
-        if(open<0) return ;
-        if(s.size()==2*n){
+        if(s.length()==2*n){
             ans.push_back(s);
             return;
         }
-        if(open<n) f(n,s+"(",open+1,close);
-        if(close<open)f(n,s+")",open,close+1);
-            
+        if(open<n) f(n,s+'(',open+1,close);
+        if(close<open) f(n,s+')',open,close+1);
     }
     vector<string> generateParenthesis(int n) {
         f(n,"",0,0);
         return ans;
-
     }
 };
